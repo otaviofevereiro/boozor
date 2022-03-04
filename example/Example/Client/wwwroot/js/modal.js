7fc868b65013729e9ae6340a3842f3fe49cd337a
Otavio Fevereiro <otavio.fevereiro@ambevtech.com.br>
2022-03-04 16:23:35 -0300

    update modal

---
+++ example/Example/Client/wwwroot/js/modal.js
@@ -0,0 +1,11 @@
+﻿var myModal;
+
+export function toggle(element) {
+    if (myModal == null) {
+        myModal = new bootstrap.Modal(element, {
+            keyboard: false
+        });
+    }
+
+    myModal.toggle();
+}
