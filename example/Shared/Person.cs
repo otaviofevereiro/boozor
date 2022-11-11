aba49dff217a23c93e6021e481e3b8fd1d139582
otavio-fevereiro <otavio.fevereiro@nubank.com.br>
2022-11-11 16:31:16 -0300

    add validation

--- example/Shared/Person.cs
+++ example/Shared/Person.cs
@@ -1,9 +1,54 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
 
 namespace Example.Shared
 {
-    public class Person
+
+
+
+    public abstract class Entity<TEntity> : IValidatableObject
+        where TEntity : Entity<TEntity>
+    {
+        private List<ValidationResult> _validations = new();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            _validations.Clear();
+            Validate();
+
+            return _validations;
+        }
+
+        protected void AddError<TKey>(Expression<Func<TEntity, TKey>> field, string errorMessage)
+        {
+            _validations.Add(new(errorMessage, new[] { GetName(field) }));
+        }
+        
+        protected abstract void Validate();
+
+        //TODO: remove to devpack
+        private string GetName<TKey>(Expression<Func<TEntity, TKey>> action)
+        {
+            return GetNameFromMemberExpression(action.Body);
+        }
+
+        //TODO: remove to devpack
+        private string GetNameFromMemberExpression(Expression expression)
+        {
+            if (expression is MemberExpression memberExpression)
+                return memberExpression.Member.Name;
+            else if (expression is UnaryExpression unaryExpression)
+            {
+                return GetNameFromMemberExpression(unaryExpression.Operand);
+            }
+
+            return "MemberNameUnknown";
+        }
+    }
+
+
+    public class Person : Entity<Person>
     {
         [Key]
         [Required]
@@ -16,11 +61,16 @@ namespace Example.Shared
         [Display(Name = "Current Email")]
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required]
         [StringLength(60)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
+        protected override void Validate()
+        {
+            if (Name == "otavio")
+                AddError(p => p.Name, "otavio nao pode");
+        }
     }
 }
