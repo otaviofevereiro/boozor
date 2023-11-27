f9e55c3fa8900f153fede8dfc8f0ce61e1999356
otavio-fevereiro <otavio.fevereiro@nubank.com.br>
2023-11-27 10:14:51 -0300

    simplify acess on fields

--- src/Boozor.Shared/Extensions.cs
+++ src/Boozor.Shared/Extensions.cs
@@ -5,6 +5,27 @@ namespace Boozor;
 
 public static class Extensions
 {
+    public static PropertyInfo GetPropertyInfo<TReturn>(this Expression<Func<TReturn>> property)
+    {
+        LambdaExpression lambda = property;
+        MemberExpression expression;
+
+        if (lambda.Body is UnaryExpression unaryExpression)
+        {
+            expression = (MemberExpression)unaryExpression.Operand;
+        }
+        else if (lambda.Body is MemberExpression memberExpression)
+        {
+            expression = memberExpression;
+        }
+        else
+        {
+            expression = (MemberExpression)lambda.Body;
+        }
+
+        return (PropertyInfo)expression.Member;
+    }
+
     //TODO:refactor
     public static PropertyInfo GetPropertyInfo<TType, TReturn>(this Expression<Func<TType, TReturn>> property)
     {
